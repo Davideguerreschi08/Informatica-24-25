7733a8649d53bbc0a1316d79eeccf5ff2629d156
Davideguerreschi08 <davide.guerreschi@galileiostiglia.edu.it>
2024-09-25 06:52:42 +0000

    esercizio di prova250924

---
+++ informatica/250924.c
@@ -0,0 +1,21 @@
+// coomento su una riga
+/* Esercizio
+	scrivere il doppio di un numero
+	fornito da tastiera
+	*/
+
+
+# include <stdio.h>
+	
+	int main(){
+		int n, doppio; /*Dichiarazione*/
+		
+		n=0; //Inizializzazione//
+		doppio=0; //Inizializzazione//
+		printf("Inserisci un numero: ",n);
+		scanf("%d", &n);
+		doppio=n*2;
+		printf("Il valore doppio è: %d",doppio);
+		return 0;
+		
+}
