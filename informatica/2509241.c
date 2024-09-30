0a4fb3fc94f4a1f80c4347b5a88b0ac60f9b1cc8
Davideguerreschi08 <davide.guerreschi@galileiostiglia.edu.it>
2024-09-30 11:36:49 +0000

    MAXminequal

--- informatica/2509241.c
+++ informatica/2509241.c
@@ -1,9 +1,25 @@
+/*Verificare il se A è il quadrato di B*/
 #include <stdio.h>
-
 int main(){
+    float A, B, quadrato;
+    float quadrato
+    float A=0;
+    float B=0;
+    quadrato= B*B;
+    printf("inserici il primo valore: ");
+    printf("inserici il secondo valore: ");
+    scanf("%f, &A &B");
+    printf("Il doppio è: %f", quadrato);
 
+    printf("Il doppio è: %f", quadrato);
 
+    if(A == quadrato){
+    printf("il primo valore %f corrisponde al quadrato di %f", A,B);
 
+    else{
+        printf("il primo valore %f non corrisponde al quadrato di %f ", A,B);
+    }
+    return 0;
 
-	
-}
+    }
+   
