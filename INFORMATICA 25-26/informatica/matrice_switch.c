a1f5ee2c348db2555f0d90dcdf65a7fda3ed054c
Davide Guerreschi <davide.guerreschi@galileiostiglia.edu.it>
2025-09-14 12:25:13 +0000

    scambio_diagonale

--- INFORMATICA 25-26/informatica/matrice_switch.c
+++ INFORMATICA 25-26/informatica/matrice_switch.c
@@ -1,24 +1,28 @@
 /*Data una matrice di valori scambiare gli elementi della diagonale principale con la diagonale secondaria.*/
 #include <stdio.h>
+void stampa_mat(int m[3][3]);
+void scambiaDiagonale(int m[3][3]);
 int main(){
     int mat[3][3]={{1,2,3},
                   {7,8,9},
                   {4,5,6}};
+
+    stampa_mat(mat);
+    scambiaDiagonale(mat);
+    stampa_mat(mat);
+}
+void stampa_mat(int m[3][3]){
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
-            printf("%d\t", mat[i][j]);
+            printf("%d\t", m[i][j]);
         }
         printf("\n");
     }
+}
+void scambiaDiagonale(int m[3][3]){
     for(int i=0;i<3;i++){
-        int temp=mat[i][i];
-        mat[i][i]=mat[i][2-i];
-        mat[i][2-i]=temp;
-    }
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            printf("%d\t", mat[i][j]);
-        }
-        printf("\n");
+        int temp=m[i][i];
+        m[i][i]=m[i][2-i];
+        m[i][2-i]=temp;
     }
 }
